637a8d82d2685e6cafbd8e4f59d42298aa8cec85
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-19 01:57:56 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
+int main(void)
+{
+	char ch = 'a';
+	char chUp = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch = ch + 1;
+	}
+	while (chUp <= 'Z')
+	{
+		putchat(chUp);
+		chUp = chUp + 1;
+	}
+	putchar('\n');
+	return (0);
+}
