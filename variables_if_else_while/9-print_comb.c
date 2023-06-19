21ba0f25591f308ed9fe5d131610bf98cc0c50b0
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-19 02:48:28 -0700

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a + '0');
+		putchar(',');
+		putchar(' ');
+		a = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
