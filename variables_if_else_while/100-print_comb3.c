81692abd1d5c7406c5945641fe296e4f5e782ef1
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-19 03:25:51 -0700

    100-print_comb3

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:always return 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a < 10)
+	{
+		b = a + 1;
+		while (b < 10)
+		{
+			if (a < b)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+			}
+			if (a != 8 || b!=9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b = b + 1;
+		}
+		a = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
