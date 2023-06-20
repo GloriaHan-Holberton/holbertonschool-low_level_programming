2ac2f57e0826b0a4ce2b7f8bacd35ead32b8ad9e
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-19 19:46:41 -0700

    3-islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - main function, entry point
+ *
+ * @c: input chararcter to compare
+ *
+ * Return: 1/0 true/false
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return ('1');
+	}
+	return ('0');
+}
