24a3c5b44541a3595d81f5c3a24a7cffc7ea449c
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-20 04:23:41 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -15,19 +15,21 @@ void times_table(void)
 		{
 			int product = n * i;
 
-			if (product < 10)
-			{
-				_putchar(product + '0');
-			} else
-			{
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
-			}
-			if (n != 9)
+			if (product < 10 && n != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
+				_putchar(product + '0');
+			} else if (product >= 10 && n != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');			}
+			if (n == 0)
+			{
+				_putchar(product + '0');
 			}
 			n = n + 1;
 		}
