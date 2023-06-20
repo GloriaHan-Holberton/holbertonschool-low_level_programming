7dab72f0803c2fdc3822789139945c014ad29205
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-20 01:01:13 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "6-abs.c"
+/**
+ * print_last_digit - main function, entry point
+ *
+ * @r: input chararcter
+ *
+ * Return: 1/0 true/false
+ */
+int print_last_digit(int r)
+{
+	_putchar('0' + _abs(r % 10));
+	return (_abs(r % 10));
+}
