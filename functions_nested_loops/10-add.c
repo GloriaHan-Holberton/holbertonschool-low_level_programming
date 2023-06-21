966ce27c3cf1cfa4a138a4291c52fa9b210028a8
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-20 17:13:37 -0700

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - main function
+ * @a: first int;
+ * @b: second int;
+ *
+ * Return: sum = a + b;
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
