f2bf2b5c22a0bc7fe65e3618c54ed90b1520e6b5
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-23 03:32:42 -0700

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		} else if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf(" Fizz");
+		} else if ((i % 5 == 0) && (i % 3 != 0))
+		{
+			printf(" Buzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+		i = i + 1;
+	}
+	printf("\n");
+	return (0);
+}
