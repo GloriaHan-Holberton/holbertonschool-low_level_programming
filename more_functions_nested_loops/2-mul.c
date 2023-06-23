5b2c7ccfcacdb054038975c08c6bfb49c0e6aabe
GloriaHan-Holberton <6723@holbertonstudents.com>
2023-06-22 18:41:22 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - main function, checks for uppercase char.
+ * @a: input parameter 1, one multiplier
+ * @b: input parameter 2, another multiplier
+ * Return: 1/0 Uppercaser/other
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
